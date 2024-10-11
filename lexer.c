dc35cddaaf8c33b61c1c1db7a99cecaa139c41ed
VladCojo <vladvcojocaru@gmail.com>
2024-10-11 16:57:24 +0300

    me forgeti

--- lexer.c
+++ lexer.c
@@ -9,6 +9,12 @@ int nTokens;
 
 int line = 1;  // the current line in the input file
 
+const char* getTokenName(int code);
+Token *addToken(int code);
+char *copyn(char *dest, const char *begin, const char *end);
+void showTokens();
+void tokenize(const char *pch);
+
 // Adds a new token to the end of tokens array and returns it
 // sets its code and line
 Token *addToken(int code) {
@@ -37,6 +43,16 @@ char *copyn(char *dest, const char *begin, const char *end) {
     return dest;
 }
 
+void showTokens(){
+    for(int i = 0; i< nTokens; i++){
+        Token *tk = &tokens[i];
+        printf("Token %d: %s (line %d)\n", i, getTokenName(tk->code), tk->line);
+        if(tk->code == ID){
+            printf("Text: %s\n", tk->text);
+        }
+    }
+}
+
 void tokenize(const char *pch) {
     const char *start;
     Token *tk;
@@ -53,10 +69,16 @@ void tokenize(const char *pch) {
 
             case '\r':
                 if (pch[1] == '\n') pch++;
+                break;
 
             case '\n':
                 line++;
                 pch++;
+                break;
+
+            case '#':
+                while(*pch != '\n' && *pch != '\0')
+                    pch++;
                 break;
 
             // DELIMITERS
@@ -98,6 +120,7 @@ void tokenize(const char *pch) {
                     addToken(ASSIGN);
                     pch++;
                 }
+                break;
 
             case '+':
                 addToken(ADD);
@@ -183,10 +206,12 @@ void tokenize(const char *pch) {
                     addToken(GREATER);
                     pch++;
                 }
+            break;
             
 
             // KEY WORDS
             default:
+                // For alphabetic key words
                 if (isalpha(*pch) || *pch == '_') {
                     for (start = pch++; isalnum(*pch) || (*pch) == '_'; pch++) {
                     }
@@ -217,8 +242,55 @@ void tokenize(const char *pch) {
                         tk = addToken(ID);
                         strcpy(tk->text, text);
                     }
+                } else {
                     pch++;
                 }
+                break;
         }
+    }
+}
+
+
+// Just for readability
+const char* getTokenName(int code) {
+    switch (code) {
+        case ID: return "ID";
+        case TYPE_INT: return "TYPE_INT";
+        case TYPE_REAL: return "TYPE_REAL";
+        case VAR: return "VAR";
+        case FUNCTION: return "FUNCTION";
+        case IF: return "IF";
+        case ELSE: return "ELSE";
+        case WHILE: return "WHILE";
+        case END: return "END";
+        case RETURN: return "RETURN";
+        case TYPE_STR: return "TYPE_STR";
+        case COMMA: return "COMMA";
+        case FINISH: return "FINISH";
+        case COLON: return "COLON";
+        case SEMICOLON: return "SEMICOLON";
+        case LPAR: return "LPAR";
+        case RPAR: return "RPAR";
+        case ADD: return "ADD";
+        case SUB: return "SUB";
+        case MUL: return "MUL";
+        case DIV: return "DIV";
+        case AND: return "AND";
+        case OR: return "OR";
+        case NOT: return "NOT";
+        case ASSIGN: return "ASSIGN";
+        case EQUAL: return "EQUAL";
+        case NOTEQ: return "NOTEQ";
+        case LESS: return "LESS";
+        case LESSEQ: return "LESSEQ";
+        case GREATER: return "GREATER";
+        case GREATEREQ: return "GREATEREQ";
+        case BITWISE_AND: return "BITWISE_AND";
+        case BITWISE_OR: return "BITWISE_OR";
+        case BITWISE_XOR: return "BITWISE_XOR";
+        case BITWISE_NOT: return "BITWISE_NOT";
+        case BITWISE_SHIFT_LEFT: return "BITWISE_SHIFT_LEFT";
+        case BITWISE_SHIFT_RIGHT: return "BITWISE_SHIFT_RIGHT";
+        default: return "UNKNOWN";
     }
 }
