5c378040c08f6629e92b0036a7c1e30743a6513c
VladCojo <vladvcojocaru@gmail.com>
2024-10-24 20:35:55 +0300

    del sum comments

--- src/main.c
+++ src/main.c
@@ -7,21 +7,8 @@
 #define MAX_INPUT_SIZE 4096
 
 int main(int argc, char* argv[]){
-    // FILE *fd = fopen(argv[1], "r");
-
-    // if(fd == NULL){
-    //     fprintf(stderr, "File not found\n");
-    //     exit(1);
-    // }
-
-    // char input[MAX_INPUT_SIZE];
-
-
-    // size_t bytes_read = fread(input, sizeof(char), MAX_INPUT_SIZE - 1, fd);
-    // input[bytes_read] = '\0';
-
-    // fclose(fd);
-    if(argc < 1){
+    
+        if(argc < 1){
         err("You must provide a path");
     }
 
