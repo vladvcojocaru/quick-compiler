a2c3bc211c621dfda02d09fc140885c3a4c1d4b5
VladCojo <vladvcojocaru@gmail.com>
2024-10-26 15:57:17 +0300

    FINALLY

--- src/parser.c
+++ src/parser.c
@@ -3,408 +3,290 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-#include "../include/lexer.h"
+#include "lexer.h"
 
-int iTk;	// the iterator in tokens
-Token *consumed;	// the last consumed token
+int iTk;  // the iterator in tokens
+Token *consumed;  // the last consumed token
 
 // same as err, but also prints the line of the current token
-_Noreturn void tkerr(const char *fmt,...){
-	fprintf(stderr,"error in line %d: ",tokens[iTk].line);
-	va_list va;
-	va_start(va,fmt);
-	vfprintf(stderr,fmt,va);
-	va_end(va);
-	fprintf(stderr,"\n");
-	exit(EXIT_FAILURE);
-	}
-
-bool consume(int code){
-
-	printf("consume(%s)\n",getTokenName(code));
-	if(tokens[iTk].code==code){
-		consumed=&tokens[iTk++];
-		return true;
-		}
-	return false;
-	}
-//
-
-// exprComp ::= exprAdd ( ( LESS | EQUAL ) exprAdd )?
-bool exprComp(){
-	int start = iTk;
-	printf("#exprComp: %s\n", getTokenName(tokens[iTk].code));
-
-	if(exprAdd()){
-		if(consume(LESS) || consume(EQUAL)){
-			if(exprAdd()){
-			} else return false;
-
-		}
-
-		return true;
-	}
-
-	iTk = start;
-	return false;
-}
-// exprAssign ::= ( ID ASSIGN )? exprComp
-bool exprAssign(){
-	int start = iTk;
-	printf("#exprAssign: %s\n", getTokenName(tokens[iTk].code));
-
-	if(consume(ID)){
-		if(consume(ASSIGN)){
-
-		} else {
-			return false;
-		}
-	}
-
-	if(exprComp()){
-		return true;
-	}
-	
-	iTk = start;
-	return false;
-}
-// exprLogic ::= exprAssign ( ( AND | OR ) exprAssign )*
-bool exprLogic(){
-	int start = iTk;
-	printf("#exprLogic: %s\n", getTokenName(tokens[iTk].code));
-
-	if(exprAssign()){
-		for(;;){
-			if(consume(AND) || consume(OR)){
-				if(exprAssign()){
-					continue;
-				}
-
-			} else break;	
-		}
-		return true;
-	}
-
-	iTk = start;
-	return false;
+_Noreturn void tkerr(const char *fmt, ...) {
+    fprintf(stderr, "error in line %d: ", tokens[iTk].line);
+    va_list va;
+    va_start(va, fmt);
+    vfprintf(stderr, fmt, va);
+    va_end(va);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
 }
 
-//expr ::= exprLogic
-bool expr(){
-	int start = iTk;
-	printf("#expr: %s\n", getTokenName(tokens[iTk].code));
-
-	if(exprLogic()){
-		return true;
-	}
-
-	iTk = start;
-	return false;
+bool consume(int code) {
+    if (tokens[iTk].code == code) {
+        consumed = &tokens[iTk++];
+        return true;
+    }
+    return false;
 }
 
-// TODO: Refactor code cause it's ugly asf
-// instr ::= expr? SEMICOLON | IF LPAR expr RPAR block ( ELSE block )? END | RETURN expr SEMICOLON | WHILE LPAR expr RPAR block END
-bool instr(){
-	int start = iTk;
-	printf("#funcParam: %s\n", getTokenName(tokens[iTk].code));
+// Function definitions
+bool consume(int code);
+bool program();
+void parse();
+bool defVar();
+bool baseType();
+bool defFunc();
+bool block();
+bool funcParams();
+bool funcParam();
+bool instr();
+bool expr();
+bool exprLogic();
+bool exprAssign();
+bool exprComp();
+bool exprAdd();
+bool exprMul();
+bool exprPrefix();
+bool factor();
 
-	if(expr()){}
-
-	if(consume(SEMICOLON)){
-		// *Copy this in else if
-		if(consume(LPAR)){
-			if(expr()){
-				if(consume(RPAR)){
-					if(block()){
-						if(consume(ELSE)){
-							if(block()){}
-							else return false;	
-						}
-						if(consume(END)){
-							if(expr()){
-								if(consume(SEMICOLON)){
-									if(consume(LPAR)){
-										if(expr()){
-											if(consume(RPAR)){
-												if(block()){
-													if(consume(END))
-														return true;
-												}
-											}
-										}
-									}
-								}
-								else if(consume(WHILE)){
-									
-									if(consume(LPAR)){
-										if(expr()){
-											if(consume(RPAR)){
-												if(block()){
-													if(consume(END))
-														return true;
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-						else if(consume(RETURN)){
-							
-							if(expr()){
-								if(consume(SEMICOLON)){
-									if(consume(LPAR)){
-										if(expr()){
-											if(consume(RPAR)){
-												if(block()){
-													if(consume(END))
-														return true;
-												}
-											}
-										}
-									}
-								}
-								else if(consume(WHILE)){
-									
-									if(consume(LPAR)){
-										if(expr()){
-											if(consume(RPAR)){
-												if(block()){
-													if(consume(END))
-														return true;
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	else if(consume(IF)){
-
-		if(consume(LPAR)){
-			if(expr()){
-				if(consume(RPAR)){
-					if(block()){
-						if(consume(ELSE)){
-							if(block()){}
-							else return false;	
-						}
-						if(consume(END)){
-							if(expr()){
-								if(consume(SEMICOLON)){
-									if(consume(LPAR)){
-										if(expr()){
-											if(consume(RPAR)){
-												if(block()){
-													if(consume(END))
-														return true;
-												}
-											}
-										}
-									}
-								}
-								else if(consume(WHILE)){
-									
-									if(consume(LPAR)){
-										if(expr()){
-											if(consume(RPAR)){
-												if(block()){
-													if(consume(END))
-														return true;
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-						else if(consume(RETURN)){
-							
-							if(expr()){
-								if(consume(SEMICOLON)){
-									if(consume(LPAR)){
-										if(expr()){
-											if(consume(RPAR)){
-												if(block()){
-													if(consume(END))
-														return true;
-												}
-											}
-										}
-									}
-								}
-								else if(consume(WHILE)){
-									
-									if(consume(LPAR)){
-										if(expr()){
-											if(consume(RPAR)){
-												if(block()){
-													if(consume(END))
-														return true;
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	iTk = start;
-	return false;
+// program ::= ( defVar | defFunc | block )* FINISH
+bool program() {
+    for (;;) {
+        if (defVar()) {}
+        else if (defFunc()) {}
+        else if (block()) {}
+        else break;
+    }
+    if (consume(FINISH)) {
+        return true;
+    } else tkerr("syntax error");
+    return false;
 }
 
-// funcParam ::= ID COLON baseType
-bool funcParam(){
-	int start = iTk;
-	printf("#funcParam: %s\n", getTokenName(tokens[iTk].code));
-
-	if(consume(ID)){
-		if(consume(COLON)){
-			if(baseType()){
-				return true;
-			}
-		}
-	}
-
-	iTk = start;
-	return false;
-}
-
-// funcParams ::= funcParam ( COMMA funcParam )*
-bool funcParams(){
-	int start = iTk;
-	printf("#funcParams: %s\n", getTokenName(tokens[iTk].code));
-	
-	if(funcParam()){
-		for(;;){
-			if(consume(COMMA)){
-				if(funcParam()){} 
-				else
-				return false;
-			}
-		}
-		return true;
-	}
-
-	iTk = start;
-	return false;
-}
-
-// block ::= instr+
-bool block(){
-	int start = iTk;
-	printf("#block: %s\n", getTokenName(tokens[iTk].code));
-
-	if(instr()){
-		for(;;){
-			if(instr()){}
-			else break;
-		}
-		return true;
-	}
-
-	iTk = start;
-	return false;
-}
-
-// defFunc ::= FUNCTION ID LPAR funcParams? RPAR COLON baseType defVar* block END
-bool defFunc(){
-	int start = iTk;
-	printf("#defFunc: %s\n", getTokenName(tokens[iTk].code));
-
-	if(consume(FUNCTION)){
-		if(consume(ID)){
-			if(consume(LPAR)){
-				if(funcParams()){}
-				if(consume(RPAR)){
-					if(consume(COLON)){
-						if(baseType()){
-							for(;;){
-								if(defVar()){}
-								else break;
-							}
-							if(block()){
-								if(consume(END)){
-									return true;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	iTk = start;
-	return false;
-}
-
-// baseType ::= TYPE_INT | TYPE_REAL | TYPE_STR
-bool baseType(){
-	int start = iTk;
-	printf("#baseType: %s\n", getTokenName(tokens[iTk].code));
-
-	if(consume(TYPE_INT))
-		return true;
-	else if(consume(TYPE_REAL))
-		return true;
-	else if(consume(TYPE_STR))
-		return true;
-	
-	iTk = start;
-	return false;
+void parse() {
+    iTk = 0;
+    program();
 }
 
 // defVar ::= VAR ID COLON baseType SEMICOLON
-bool defVar(){
-	int start = iTk;
-	printf("#defVar: %s\n", getTokenName(tokens[iTk].code));
-
-	if (!consume(VAR)) {
-        tkerr("Expected 'VAR' at the beginning of a variable declaration");
+bool defVar() {
+    if (consume(VAR)) {
+        if (consume(ID)) {
+            if (consume(COLON)) {
+                if (baseType()) {
+                    if (consume(SEMICOLON)) {
+                        return true;
+                    } else tkerr("Missing ';' after variable declaration");
+                }
+            } else tkerr("Missing ':' in variable declaration");
+        } else tkerr("Missing identifier in variable declaration");
     }
-    if (!consume(ID)) {
-        tkerr("Expected identifier after 'VAR'");
-    }
-    if (!consume(COLON)) {
-        tkerr("Expected ':' after identifier in variable declaration");
-    }
-    if (!baseType()) {
-        tkerr("Expected base type after ':' in variable declaration");
-    }
-    if (!consume(SEMICOLON)) {
-        tkerr("Expected ';' at the end of variable declaration");
-    }
-
-	iTk = start;
-	return false;
+    return false;
 }
 
-//* Helper function
-bool processTopLevelElement(){
-	return defVar() || defFunc() || block();
-}
-// program ::= ( defVar | defFunc | block )* FINISH
-bool program(){
-	while (processTopLevelElement())
-
-	if(consume(FINISH))
-		return true;
-	else 
-		tkerr("syntax error");
-		
+// baseType ::= TYPE_INT | TYPE_REAL | TYPE_STR
+bool baseType() {
+    if (consume(TYPE_INT) || consume(TYPE_REAL) || consume(TYPE_STR)) {
+        return true;
+    }
+    return false;
 }
 
-void parse(){
-	iTk=0;
-	program();
-	}
+// defFunc ::= FUNCTION ID LPAR funcParams? RPAR COLON baseType defVar* block END
+bool defFunc() {
+    if (consume(FUNCTION)) {
+        if (consume(ID)) {
+            if (consume(LPAR)) {
+                funcParams();
+                if (consume(RPAR)) {
+                    if (consume(COLON)) {
+                        if (baseType()) {
+                            while (defVar()) {}
+                            if (block()) {
+                                if (consume(END)) {
+                                    return true;
+                                } else tkerr("Missing 'END' after function definition");
+                            }
+                        }
+                    } else tkerr("Missing ':' in function declaration");
+                } else tkerr("Missing ')' after function parameters");
+            } else tkerr("Missing '(' after function identifier");
+        } else tkerr("Missing function identifier");
+    }
+    return false;
+}
+
+// block ::= instr+
+bool block() {
+    if (instr()) {
+        while (instr()) {}
+        return true;
+    }
+    return false;
+}
+
+// funcParams ::= funcParam ( COMMA funcParam )*
+bool funcParams() {
+    if (funcParam()) {
+        while (consume(COMMA)) {
+            if (!funcParam()) tkerr("Invalid function parameter after ','");
+        }
+        return true;
+    }
+    return false;
+}
+
+// funcParam ::= ID COLON baseType
+bool funcParam() {
+    if (consume(ID)) {
+        if (consume(COLON)) {
+            if (baseType()) {
+                return true;
+            } else tkerr("Invalid base type in function parameter");
+        } else tkerr("Missing ':' in function parameter");
+    }
+    return false;
+}
+
+// instr ::= expr? SEMICOLON | IF LPAR expr RPAR block ( ELSE block )? END | RETURN expr SEMICOLON | WHILE LPAR expr RPAR block END
+bool instr() {
+    int start = iTk;
+    if (expr()) {
+        if (consume(SEMICOLON)) {
+            return true;
+        }
+        iTk = start;
+    }
+    if (consume(IF)) {
+        if (consume(LPAR)) {
+            if (expr()) {
+                if (consume(RPAR)) {
+                    if (block()) {
+                        if (consume(ELSE)) {
+                            if (!block()) tkerr("Expected block after 'else'");
+                        }
+                        if (consume(END)) {
+                            return true;
+                        } else tkerr("Missing 'END' after 'if' statement");
+                    }
+                }
+            }
+        }
+    } else if (consume(RETURN)) {
+        if (expr()) {
+            if (consume(SEMICOLON)) {
+                return true;
+            } else tkerr("Missing ';' after return statement");
+        } else tkerr("Missing expression in return statement");
+    } else if (consume(WHILE)) {
+        if (consume(LPAR)) {
+            if (expr()) {
+                if (consume(RPAR)) {
+                    if (block()) {
+                        if (consume(END)) {
+                            return true;
+                        } else tkerr("Missing 'END' after 'while' loop");
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+// expr ::= exprLogic
+bool expr() {
+    return exprLogic();
+}
+
+// exprLogic ::= exprAssign ( ( AND | OR ) exprAssign )*
+bool exprLogic() {
+    if (exprAssign()) {
+        while (consume(AND) || consume(OR)) {
+            if (!exprAssign()) tkerr("Invalid expression after 'and/or'");
+        }
+        return true;
+    }
+    return false;
+}
+
+// exprAssign ::= ( ID ASSIGN )? exprComp
+bool exprAssign() {
+    int start = iTk;
+    if (consume(ID)) {
+        if (consume(ASSIGN)) {
+            if (exprComp()) {
+                return true;
+            } else tkerr("Invalid expression after '='");
+        }
+        iTk = start;
+    }
+    return exprComp();
+}
+
+// exprComp ::= exprAdd ( ( LESS | EQUAL ) exprAdd )?
+bool exprComp() {
+    if (exprAdd()) {
+        if (consume(LESS) || consume(EQUAL)) {
+            if (!exprAdd()) tkerr("Invalid expression after '<' or '='");
+        }
+        return true;
+    }
+    return false;
+}
+
+// exprAdd ::= exprMul ( ( ADD | SUB ) exprMul )*
+bool exprAdd() {
+    if (exprMul()) {
+        while (consume(ADD) || consume(SUB)) {
+            if (!exprMul()) tkerr("Invalid expression after '+' or '-'");
+        }
+        return true;
+    }
+    return false;
+}
+
+// exprMul ::= exprPrefix ( ( MUL | DIV ) exprPrefix )*
+bool exprMul() {
+    if (exprPrefix()) {
+        while (consume(MUL) || consume(DIV)) {
+            if (!exprPrefix()) tkerr("Invalid expression after '*' or '/'");
+        }
+        return true;
+    }
+    return false;
+}
+
+// exprPrefix ::= ( SUB | NOT )? factor
+bool exprPrefix() {
+    if (consume(SUB) || consume(NOT)) {
+        return factor();
+    }
+    return factor();
+}
+
+// factor ::= INT | REAL | STR | LPAR expr RPAR | ID ( LPAR ( expr ( COMMA expr )* )? RPAR )?
+bool factor() {
+    if (consume(INT) || consume(REAL) || consume(STRING)) {
+        return true;
+    }
+    if (consume(LPAR)) {
+        if (expr()) {
+            if (consume(RPAR)) {
+                return true;
+            } else tkerr("Missing ')' after expression");
+        }
+    }
+    if (consume(ID)) {
+        if (consume(LPAR)) {
+            if (expr()) {
+                while (consume(COMMA)) {
+                    if (!expr()) tkerr("Invalid expression after ',' in function call");
+                }
+            }
+            if (consume(RPAR)) {
+                return true;
+            } else tkerr("Missing ')' after function arguments");
+        }
+        return true;
+    }
+    return false;
+}
