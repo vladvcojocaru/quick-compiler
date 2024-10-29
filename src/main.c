6417c6c3eb35ea0420b5112e04123aa92657e2de
VladCojo <vladvcojocaru@gmail.com>
2024-10-29 22:19:15 +0200

    just sum stuff

--- src/main.c
+++ src/main.c
@@ -9,19 +9,18 @@
 #define MAX_INPUT_SIZE 4096
 
 int main(int argc, char* argv[]){
-    
+        
         if(argc < 1){
         err("You must provide a path");
     }
 
     char *buf = loadFile(argv[1]);
-    
     tokenize(buf);
     parse();
 
     printf("Tokens: \n");
     showTokens();
 
-
+    
     return EXIT_SUCCESS;
 }
