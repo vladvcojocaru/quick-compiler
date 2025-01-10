c1a54984d6bfff3f463ffcabcb40d449379af012
vladvcojocaru <vladvcojocaru@gmail.com>
2025-01-10 09:20:47 +0200

    eliminate some unused includes

--- 1.c
+++ 1.c
@@ -1,22 +1,23 @@
-#include "quick.h"
+#include"quick.h"
 
 int i;
 
-int max(int x, int y) {
-    if (x < y) {
-        return y;
-    } else {
-        return x;
-    }
+int max(int x,int y){
+if(x<y){
+return y;
+}
+else{
+return x;
+}
 }
 
-int main() {
-    i = 0;
-    while (i < 10) {
-        puti(max(i, 5));
-        i = i + 1;
-    }
-    puts("PI=");
-    putr(3.14159);
-    return 0;
+int main(){
+i=0;
+while(i<10){
+puti(max(i,5));
+i=i+1;
+}
+puts("PI=");
+putr(3.14159);
+return 0;
 }
--- src/gen.c
+++ src/gen.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../include/ad.h"
 #include "../include/gen.h"
 #include "../include/lexer.h"
 
